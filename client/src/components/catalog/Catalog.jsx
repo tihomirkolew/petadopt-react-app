df97a0c7e0e189b1e267a5d2572dc9058b70aaaf
Tihomir Kolev <tihomirkolew@gmail.com>
2025-12-07 14:08:37 +0200

    Update Catalog component to enhance pet fetching and display functionality

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
+import PetCard from "../petCard/PetCard";
+
 export default function Catalog() {
+
+    const [pets, setPets] = useState([]);
+
+    const searchParams = encodeURIComponent('_createdOn desc');
+
+    useEffect(() => {
+        fetch(`http://localhost:3030/data/pets?sortBy=${searchParams}`)
+            .then(res => res.json())
+            .then(data => {
+                setPets(data);
+            })
+            .catch(err => {
+                console.error('Error fetching latest pets:', err);
+            });
+
+    }, [searchParams]);
+
     return (
         <>
-            <div className="container-fluid tm-container-content tm-mt-60" style={{ minHeight: '78vh' }}>
+            <div className="container-fluid tm-container-content tm-mt-60"
+                style={{ minHeight: '78vh' }}>
                 {/* Page title */}
                 <div className="row mb-4">
                     <h2 className="col-6 tm-text-primary">
@@ -10,60 +31,17 @@ export default function Catalog() {
                 </div>
 
                 {/* Items */}
-                <div className="row tm-mb-90 tm-gallery">
-                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
-                        <figure className="effect-ming tm-video-item">
-                            <img src="/images/img-05.jpg" alt="Image" className="img-fluid" />
-                            <figcaption className="d-flex align-items-center justify-content-center">
-                                <h2 className="col-6 tm-text-primary">Clocks</h2>
-                                <a href="photo-detail.html">View more</a>
-                            </figcaption>
-                        </figure>
-                        <div className="d-flex justify-content-between tm-text-gray">
-                            <span className="tm-text-gray-light">18 Oct 2020</span>
-                            {/* <span>9,906 views</span> */}
-                        </div>
-                    </div>
-                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
-                        <figure className="effect-ming tm-video-item">
-                            <img src="/images/img-05.jpg" alt="Image" className="img-fluid" />
-                            <figcaption className="d-flex align-items-center justify-content-center">
-                                <h2 className="col-6 tm-text-primary">Clocks</h2>
-                                <a href="photo-detail.html">View more</a>
-                            </figcaption>
-                        </figure>
-                        <div className="d-flex justify-content-between tm-text-gray">
-                            <span className="tm-text-gray-light">18 Oct 2020</span>
-                            {/* <span>9,906 views</span> */}
-                        </div>
-                    </div>
-                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
-                        <figure className="effect-ming tm-video-item">
-                            <img src="/images/img-05.jpg" alt="Image" className="img-fluid" />
-                            <figcaption className="d-flex align-items-center justify-content-center">
-                                <h2 className="col-6 tm-text-primary">Clocks</h2>
-                                <a href="photo-detail.html">View more</a>
-                            </figcaption>
-                        </figure>
-                        <div className="d-flex justify-content-between tm-text-gray">
-                            <span className="tm-text-gray-light">18 Oct 2020</span>
-                            {/* <span>9,906 views</span> */}
-                        </div>
-                    </div>
-                    <div className="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
-                        <figure className="effect-ming tm-video-item">
-                            <img src="/images/img-05.jpg" alt="Image" className="img-fluid" />
-                            <figcaption className="d-flex align-items-center justify-content-center">
-                                <h2 className="col-6 tm-text-primary">Clocks</h2>
-                                <a href="photo-detail.html">View more</a>
-                            </figcaption>
-                        </figure>
-                        <div className="d-flex justify-content-between tm-text-gray">
-                            <span className="tm-text-gray-light">18 Oct 2020</span>
-                            {/* <span>9,906 views</span> */}
-                        </div>
-                    </div>
-
+                <div className="row tm-mb-90 tm-gallery" style={{
+                    display: "flex",
+                    flexWrap: "nowrap",
+                    overflowX: "auto",
+                    gap: "1rem"
+                }}>
+                    {pets.length > 0 ? (
+                        pets.map(pet => <PetCard key={pet._id} {...pet} />)
+                    ) : (
+                        <p>No pets found.</p>
+                    )}
                 </div>
             </div>
         </>
