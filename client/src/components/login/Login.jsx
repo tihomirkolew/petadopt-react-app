bcd7bd1cf22c33a9a8f002f09b025ffedd3e1ed5
Tihomir Kolev <sineadfly@gmail.com>
2025-12-09 20:10:10 +0200

    Implement login form submission handling and integrate user context

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,9 +1,32 @@
+import { useNavigate } from "react-router";
+import { useUserContext } from "../../contexts/UserContext";
+
 export default function Login() {
+
+    const { loginHandler } = useUserContext();
+    const navigate = useNavigate();
+
+    const loginSubmitHandler = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+
+        const email = formData.get('email').trim();
+        const password = formData.get('password').trim();
+
+        try {
+            await loginHandler(email, password);
+            navigate('/');
+        } catch (err) {
+            return { success: false, message: err.message };
+        }
+    }
+
+
     return (
         <>
             <div className="col-lg-4 col-12 mb-5 mx-auto" style={{minHeight: '74vh'}}>
                 <h2 className="tm-text-primary pt-5 mb-5 text-center">Login</h2>
-                <form id="login-form" action="" method="POST" className="tm-contact-form mx-auto">
+                <form id="login-form" action="" method="POST" className="tm-contact-form mx-auto" onSubmit={loginSubmitHandler}>
                     <div className="form-group">
                         <input type="email" name="email" className="form-control rounded-0" placeholder="Email" required />
                     </div>
