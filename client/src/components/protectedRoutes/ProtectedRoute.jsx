dcbb9e2d0d36c9a582efad1d1e0c0d2ebd5ca84b
Tihomir Kolev <sineadfly@gmail.com>
2025-12-16 22:23:19 +0200

    Fix ProtectedRoute logic to correctly render Outlet for authenticated users

--- client/src/components/protectedRoutes/ProtectedRoute.jsx
+++ client/src/components/protectedRoutes/ProtectedRoute.jsx
@@ -1,10 +1,9 @@
-import { Navigate } from "react-router";
+import { Navigate, Outlet } from "react-router";
 import { useUserContext } from "../../contexts/UserContext";
 
 export default function ProtectedRoute() {
 
   const { isAuthenticated } = useUserContext();
 
-  
-    return !isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 }
