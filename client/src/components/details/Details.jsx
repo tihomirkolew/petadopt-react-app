746610f24a14c38c60d088494772fda4d7f735af
Tihomir Kolev <sineadfly@gmail.com>
2025-12-17 19:09:55 +0200

    Refactor Details component to use CSS modules

--- client/src/components/details/Details.jsx
+++ client/src/components/details/Details.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link, Navigate, useNavigate, useParams } from "react-router";
 import { useUserContext } from "../../contexts/UserContext";
 import usePetRequest from "../../hooks/usePetRequest";
+import styles from './Details.module.css'
 
 export default function Details() {
     const { user } = useUserContext();
@@ -88,17 +89,19 @@ export default function Details() {
 
     return (
         <>
-            <div className="container-fluid tm-container-content tm-mt-40 tm-mb-60>" style={{ minHeight: '75vh', flexBasis: '1 0 0' }}>
+            <div className={`container-fluid tm-container-content tm-mt-40 tm-mb-60 ${styles.height}`}>
                 <div className="row justify-content-center" >
                     <div className="col-xl-8 col-lg-7 col-md-6 col-sm-12">
-                        <img src={pet.imageUrl || "/images/image-placeholder.png"} alt="Image" className="img-fluid"
-                            style={{ width: '93%', height: 'auto' }}
+                        <img src={pet.imageUrl || "/images/image-placeholder.png"}
+                            alt="Image"
+                            className={`img-fluid ${styles["smaller-images"]}`}
+                            // style={{ width: '93%', height: 'auto' }}
                         />
                     </div>
 
-                    <div className="col-xl-4 col-lg-5 col-md-6 col-sm-12">
+                    <div className={`col-xl-4 col-lg-5 col-md-6 col-sm-12 ${styles.info}`}>
                         <div className="tm-bg-gray tm-video-details d-flex flex-column justify-content-between">
-                            <div className="mb-4" style={{ flexGrow: 1 }}>
+                            <div className={`mb-4 ${styles["details-body"]}`}>
                                 <div className="row mb-4">
                                     <h1 className="col-12 tm-text-primary">{pet?.name}</h1>
                                 </div>
@@ -107,9 +110,9 @@ export default function Details() {
                                 <div>
                                     <h3 className="tm-text-gray-dark mb-3">Kind: <span className="tm-text-primary">{pet?.kind}</span></h3>
                                 </div>
-                                <p>{pet?.description}</p>
+                                <p className={styles.description}>{pet?.description}</p>
                                 <div>
-                                    <h3 className="tm-text-gray-dark mb-3">Contact: <span className="tm-text-primary">{pet?.contact}</span></h3>
+                                    <h3 className="tm-text-gray-dark">Contact: <span className="tm-text-primary">{pet?.contact}</span></h3>
                                 </div>
                             </div>
                             <div className="d-flex justify-content-between mt-4">
--- client/src/components/editPet/EditPet.jsx
+++ client/src/components/editPet/EditPet.jsx
@@ -38,6 +38,10 @@ export default function EditPet() {
             errors.description = 'Description must be at least 10 characters';
         }
 
+        if (values.description.trim().length > 700) {
+            errors.description = 'Description too long'
+        }
+
         try {
             new URL(values.imageUrl);
         } catch {
